8b567a24bb87fbe6da46b065d05435a574231d7c
davide <davide.magagni@gmail.com>
2022-03-16 16:25:11 +0000

    bug fix2

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Post
+from .models import Professor
+from .models import Subject
 
 admin.site.register(Post)
+admin.site.register(Professor)
+admin.site.register(Subject)
 
 # Register your models here.
